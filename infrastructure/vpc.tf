62912d2577e447fee2ae0dc6901ed2fcc0dae20a
Israel Tomilayo <itomilayo@gmail.com>
2020-11-20 17:34:55 +0100

    create an internet gateway and add to route table

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -148,3 +148,17 @@ resource "aws_route" "nat-gw-route" {
   nat_gateway_id         = "${aws_nat_gateway.nat-gw.id}"
   destination_cidr_block = "0.0.0.0/0"
 }
+
+resource "aws_internet_gateway" "production-igw" {
+  vpc_id = "${aws_vpc.production-vpc.id}"
+
+  tags {
+    Name = "Production-IGW"
+  }
+}
+
+resource "aws_route" "public-internet-gw-route" {
+  route_table_id         = "${aws_route_table.public-route-table.id}"
+  gateway_id             = "${aws_internet_gateway.production-igw.id}"
+  destination_cidr_block = "0.0.0.0/0"
+}
