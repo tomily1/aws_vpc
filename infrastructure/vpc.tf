2458421d64f64d4c5f2c4e34dbe88837cc5f17c3
Israel Tomilayo <itomilayo@gmail.com>
2020-11-20 17:29:46 +0100

    create NAT gateway and add to route table

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -124,10 +124,27 @@ resource "aws_route_table_association" "private-subnet-3-association" {
 }
 
 resource "aws_eip" "elastic-ip-for-nat-gw" {
-  vpc = true
+  vpc                       = true
   associate_with_private_ip = "10.0.0.5"
 
   tags {
     Name = "Production-EIP"
   }
+}
+
+resource "aws_nat_gateway" "nat-gw" {
+  allocation_id = "${aws_eip.elastic-ip-for-nat-gw.id}"
+  subnet_id = "${aws_subnet.public-subnet-1.id}"
+
+  tags {
+    Name = "Production-NAT-GW"
+  }
+
+  depends_on = ["aws_eip.elastic-ip-for-nat-gw"]
+}
+
+resource "aws_route" "nat-gw-route" {
+  route_table_id         = "${aws_route_table.private-route-table.id}"
+  nat_gateway_id         = "${aws_nat_gateway.nat-gw.id}"
+  destination_cidr_block = "0.0.0.0/0"
 }
