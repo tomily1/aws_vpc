945b8dfeeb971c6e6bc6289806d3189b6a913855
Israel Tomilayo <itomilayo@gmail.com>
2020-11-20 17:03:53 +0100

    create private subnets

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -44,3 +44,33 @@ resource "aws_subnet" "public-subnet-3" {
     Name = "Public-Subnet-3"
   }
 }
+
+resource "aws_subnet" "private-subnet-1" {
+  cidr_block        = "${var.private_subnet_1_cidr}"
+  vpc_id            = "${aws_vpc.production-vpc.id}"
+  availability_zone = "eu-west-1a"
+
+  tags = {
+    Name = "Private-Subnet-1"
+  }
+}
+
+resource "aws_subnet" "private-subnet-2" {
+  cidr_block        = "${var.private_subnet_2_cidr}"
+  vpc_id            = "${aws_vpc.production-vpc.id}"
+  availability_zone = "eu-west-1b"
+
+  tags = {
+    Name = "Private-Subnet-2"
+  }
+}
+
+resource "aws_subnet" "private-subnet-3" {
+  cidr_block        = "${var.private_subnet_3_cidr}"
+  vpc_id            = "${aws_vpc.production-vpc.id}"
+  availability_zone = "eu-west-1c"
+
+  tags = {
+    Name = "Private-Subnet-3"
+  }
+}
