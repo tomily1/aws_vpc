73bf2c412572957473cc5e40ce8b41e2d5624ebe
Israel Tomilayo <itomilayo@gmail.com>
2020-12-02 17:20:37 +0100

    define autoscaling notifications for triggering certain events

--- instances/main.tf
+++ instances/main.tf
@@ -331,3 +331,13 @@ resource "aws_sns_topic_subscription" "webapp_production_autoscaling_sms_subscri
   protocol = "sms"
   topic_arn = aws_sns_topic.webapp_production_autoscaling_alert_topic.arn
 }
+
+resource "aws_autoscaling_notification" "webapp_production_autoscaling_notification" {
+  group_names   = [ aws_autoscaling_group.ec2_public_autoscaling_group.name ]
+  notifications = [
+    "autoscaling:EC2_INSTANCE_LAUNCH",
+    "autoscaling:EC2_INSTANCE_TERMINATE",
+    "autoscaling:EC2_INSTANCE_LAUNCH_ERROR"
+  ]
+  topic_arn = aws_sns_topic.webapp_production_autoscaling_alert_topic.arn
+}
